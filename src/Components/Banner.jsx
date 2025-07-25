29e4681bc7ce7be122bc55f019c3e2d59cf2c5cb
Gerald6025 <geraldgchibanda6025@gmail.com>
2025-07-25 10:47:39 -0300

    adde banner image

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row h-auto md:h-[83vh] bg-white w-full flex-wrap">
-        
         <div className="ml-4 md:ml-15 pt-6 md:pt-45 mt-3 bg-[url(./assets/Ab.PNG)] bg-center bg-cover bg-gray-200 bg-blend-overlay h-auto md:h-120 w-full md:w-1/2 p-4 md:p-0">
           <p className="pb-3 text-[14px] md:text-[16px] font-serif">
             "Nurturing Young Minds, Building a Bright Future"
@@ -30,14 +29,20 @@ const Banner = () => {
         </div>
 
         <div className="mt-6 md:mt-[80px] w-full md:w-[450px] h-auto md:h-[612px] ml-0 md:ml-10 flex justify-center">
-          <img className="w-[90%] md:w-full h-120 object-contain  " src="src/assets/Banner.png" alt="banner" />
+          <img
+            className="w-[90%] md:w-full h-120 object-contain  "
+            src="images/Banner.png"
+            alt="banner"
+          />
         </div>
       </div>
 
       <div className="flex justify-center md:justify-evenly flex-wrap gap-4 md:gap-0 mt-6 md:mt-10 px-4">
         <div className="flex items-center gap-2 w-full sm:w-auto">
           <img src="src/assets/assessment.png" alt="assesment" />
-          <p className="text-[14px] md:text-[15px]">Assessments & Certification</p>
+          <p className="text-[14px] md:text-[15px]">
+            Assessments & Certification
+          </p>
         </div>
 
         <div className="flex items-center gap-2 w-full sm:w-auto">
@@ -47,12 +52,16 @@ const Banner = () => {
 
         <div className="flex items-center gap-2 w-full sm:w-auto">
           <img src="src/assets/Practical.png" alt="assesment" />
-          <p className="text-[14px] md:text-[15px]">Practical & Life Skill Subjects</p>
+          <p className="text-[14px] md:text-[15px]">
+            Practical & Life Skill Subjects
+          </p>
         </div>
 
         <div className="flex items-center gap-2 w-full sm:w-auto">
           <img src="src/assets/creative.png" alt="assesment" />
-          <p className="text-[14px] md:text-[15px]">Creative Arts & Physical Education</p>
+          <p className="text-[14px] md:text-[15px]">
+            Creative Arts & Physical Education
+          </p>
         </div>
       </div>
     </>
