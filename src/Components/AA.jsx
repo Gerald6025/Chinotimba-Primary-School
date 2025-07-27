a9f68df96d3b5d282a34474a61308a4a8c35f457
Gerald6025 <geraldgchibanda6025@gmail.com>
2025-07-27 19:52:42 -0300

    academic redo

--- src/Components/AA.jsx
+++ src/Components/AA.jsx
@@ -37,17 +37,17 @@ const AA = () => {
   ];
 
   return (
-    <>
-      <div className="mt-30 ml-10 h-10 ">
+    <div>
+      <div className="mt-30 ml-10 h-10  ">
         <h1 className="md:w-[400px] h-[30px] md:text-[30px] text-[25px] w-[300px] ">Academic Achievements</h1>
         <hr className="bg-Helenmaroon w-[310px] h-[3px] mt-3" />
       </div>
 
-      <div ref={ref} className="flex justify-evenly flex-wrap gap-10 h-auto mt-10 px-4 ">
+      <div ref={ref} className="flex justify-evenly flex-wrap md:gap-10  gap-0 h-auto  mt-10 px-4 ">
         {Archives.map((archive) => (
           <div
             key={archive.id}
-            className={`relative w-[361px] h-[300px] border-Helenmaroon border-[1px] rounded-bl-[10px] rounded-tl-[60px] rounded-br-[60px] rounded-tr-[10px] flex flex-col justify-center items-center mb-32 mt-15 text-center gap-7 p-4 ${archive.bgClass}`}
+            className={`relative md:w-[361px] w-[320px] h-[300px] border-Helenmaroon border-[1px] rounded-bl-[10px] rounded-tl-[60px] rounded-br-[60px] rounded-tr-[10px] flex flex-col justify-center items-center mb-32 mt-15 text-center gap-7 p-4 ${archive.bgClass}`}
           >
           
             <div className="absolute top-0 right-0 translate-x-1/2  rotate-y-180  -translate-y-1/2  md:w-12 md:h-12 w-10 h-10 bg-Helenmaroon text-white border-4 border-white rounded-tl-3xl flex justify-center items-center z-50">
@@ -72,7 +72,7 @@ const AA = () => {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
