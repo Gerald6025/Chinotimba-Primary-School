3476dcd104ca1208445fd7d17eb388952db96e5a
Gerald6025 <geraldgchibanda6025@gmail.com>
2025-07-27 22:27:28 -0300

    gallery

--- src/Components/GalHero.jsx
+++ src/Components/GalHero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const GalHero = () => {
   return (
     <div className='w-full h-[550px] bg-gradient-to-r from-Helenmaroon to-[#CA1E49]  flex flex-col items-center text-center '>
-      <h1 className='w-[835px] h-[50px] text-[50px] pt-35  text-white '>Gallery-Chinotimba Primary School</h1>
-    <p className='w-[835px] h-[46px] text-[20px] pt-25  text-white '>Explore our vibrant and inspiring school environment — from classrooms and sports fields to cultural events and community projects.</p>
+      <h1 className='md:w-[835px] w-[300px] h-[50px] md:text-[50px] text-[30px] pt-35  text-white '>Gallery-Chinotimba Primary School</h1>
+    <p className=' md:w-[835px] w-[300px] h-[46px] text-[20px] pt-30  text-white '>Explore our vibrant and inspiring school environment — from classrooms and sports fields to cultural events and community projects.</p>
     </div>
   )
 }
--- src/Components/Galpix.jsx
+++ src/Components/Galpix.jsx
@@ -1,62 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Galpix = () => {
   return (
-   <div className='flex justify-evenly flex-wrap'>
-   
-   <div className='h-160 w-[50%] flex flex-wrap justify-evenly pt-10 gap-y-0 gap-x-0 items-center mb-10 '>
-      <div>
-        <img src="images/gal1.png" alt="" className='w-[295px] h-[354px] rounded-[10px] object-fit' />
-      </div>
-    
-    <div>
-        <img src="images/gal2.png" alt="" className='w-[295px] h-[354px] rounded-[10px] object-cover border-Helenmaroon border-2' />
-      </div>
-    
-    <div>
-        <img src="images/gal3.png" alt="" className='w-[295px] h-[175px] rounded-[10px] object-fit' />
-      </div>
-
-    <div>
-        <img src="images/gal4.png" alt="" className='w-[295px] h-[175px] rounded-[10px] object-cover' />
+    <div className='flex flex-wrap justify-evenly h-auto md:h-200'>
+      <div className='h-auto md:h-160 w-full md:w-[50%] flex flex-wrap justify-evenly pt-10 gap-y-0 gap-x-0 items-center mb-10'>
+        <div>
+          <img src="images/gal1.png" alt="" className='w-[295px] h-[354px] rounded-[10px] object-fit' />
+        </div>
+        <div>
+          <img src="images/gal2.png" alt="" className='w-[295px] h-[354px] rounded-[10px] object-cover border-Helenmaroon border-2 md:ml-0 md:mt-0 ml-5 mt-20' />
+        </div>
+        <div>
+          <img src="images/gal3.png" alt="" className='w-[295px] h-[175px] rounded-[10px] object-fit hidden md:block ' />
+        </div>
+        <div>
+          <img src="images/gal4.png" alt="" className='w-[295px] h-[175px] rounded-[10px] object-cover hidden md:block ' />
+        </div>
       </div>
 
+      <div className='w-full md:w-[50%] h-auto md:h-160 pt-15 flex   h flex-col md:flex-row gap-7'>
+        <div className='w-full md:w-auto hidden md:block'>
+          <img src="images/gal5.png" alt="" className='w-[295px] h-[562px] rounded-[10px] object-cover' />
+        </div>
+        <div className='flex flex-col gap-y-12 items-center justify-center w-full md:w-auto'>
+          <img src="images/gal6.png" alt="" className='w-[295px] h-[354px] rounded-[10px] object-cover' />
+          <img src="images/gal7.png" alt="" className='w-[295px] h-[225px] rounded-[10px] object-cover mb-5' />
+        </div>
+      </div>
     </div>
-
- <div className=' w-[50%] h-160 pt-15 flex gap-7'>
-
-<div>
-<img src="images/gal5.png" alt="" className='w-[295px] h-[562px] rounded-[10px] object-cover' />
-</div>
-
-
-<div className='flex flex-col gap-y-12 items-center justify-center'>
-<img src="images/gal6.png" alt="" className='w-[295px] h-[354px] rounded-[10px] object-cover' />
-<img src="images/gal7.png" alt="" className='w-[295px] h-[225px] rounded-[10px] object-cover mb-5' />
-
-</div>
-
-
-
- 
- 
- 
- 
- 
- </div>
-
-
-
-
-
-
-
-
-
-
-
-    </div>
-  )
+  );
 }
 
-export default Galpix
+export default Galpix;
